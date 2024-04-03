3a18d1ba46b620ba1171ba40d5641c7105108b57
chaosannals <chaosannals@outlook.com>
2024-04-03 13:33:40 +0800

    save

---
+++ v3demows/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func init() {
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		log.Println("CheckOrigin: true")
+		return true
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ws", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+		for {
+			conn.WriteMessage(websocket.TextMessage, []byte("Hello, WebSocket!"))
+			time.Sleep(time.Second)
+		}
+	})
+	router.Run("0.0.0.0:48080")
+}
